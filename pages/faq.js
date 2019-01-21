fffa441bdaebe82454a37b5c221ed4b5cdaaf2dc
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2019-01-21 17:12:30 +0800

    ok

--- pages/faq.js
+++ pages/faq.js
@@ -25,7 +25,7 @@ export default class extends Page {
     this.toggleFade = this.toggleFade.bind(this);
     this.state = {
       collapse: false,
-      accordion: [1, 0, 0, 0, 0, 0, 0, 0],
+      accordion: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       custom: [true, false],
       status: "Closed",
       fadeIn: true,
@@ -358,6 +358,366 @@ export default class extends Page {
                       through BDO Bank Deposit, Online Banking, Fund Transfer,
                       Remittance or Cash Payment (Office address indicated in
                       the contract).
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading9">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(8)}
+                      aria-expanded={this.state.accordion[8]}
+                      aria-controls="collapse9"
+                    >
+                      <h5 className="m-0 p-0">
+                        When is the deadline of payment?
+                      </h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[8]}
+                    data-parent="#accordion"
+                    id="collapse9"
+                  >
+                    <CardBody>
+                      From the day (date is indicated in the contract) you have
+                      received the contract, you have 3-6 banking days to settle
+                      your payment. Failure to pay automatically forfeits
+                      contract. (Note that profit distribution is on a First
+                      Pay, First Out basis)
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading10">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(9)}
+                      aria-expanded={this.state.accordion[9]}
+                      aria-controls="collapse10"
+                    >
+                      <h5 className="m-0 p-0">Acknowledgement of payment</h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[9]}
+                    data-parent="#accordion"
+                    id="collapse10"
+                  >
+                    <CardBody>
+                      Your payment will be confirmed after you send your signed
+                      contract and proof of payment. Payment will be confirmed
+                      within 5-7 working days on the same ticket where you have
+                      sent it. If you don't received any confirmation within 7
+                      working days, you may call us immediately, delays may due
+                      to a large number of tickets that we are responding.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading11">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(10)}
+                      aria-expanded={this.state.accordion[10]}
+                      aria-controls="collapse11"
+                    >
+                      <h5 className="m-0 p-0">
+                        Acknowledgement of late payment
+                      </h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[10]}
+                    data-parent="#accordion"
+                    id="collapse11"
+                  >
+                    <CardBody>
+                      Payments made after 6 banking days automatically forfeits
+                      the existing contract. Payment will automatically recorded
+                      and can be used to fund for the next cycles. Late payment
+                      can be refunded but FarmOner shall bear all costs and
+                      expenses (such as processing fee and bank charges) upon
+                      recovering payment which is 10% deducted from his/her
+                      total payment.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading12">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(11)}
+                      aria-expanded={this.state.accordion[11]}
+                      aria-controls="collapse12"
+                    >
+                      <h5 className="m-0 p-0">
+                        Receiving of notarized contract
+                      </h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[11]}
+                    data-parent="#accordion"
+                    id="collapse12"
+                  >
+                    <CardBody>
+                      Notarized Contracts will be sent at Message Centre within
+                      the month of the start of the real farming.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading13">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(12)}
+                      aria-expanded={this.state.accordion[12]}
+                      aria-controls="collapse13"
+                    >
+                      <h5 className="m-0 p-0">
+                        Return of investment differences
+                      </h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[12]}
+                    data-parent="#accordion"
+                    id="collapse13"
+                  >
+                    <CardBody>
+                      Sales profit will depend on the crops/livestocks produce,
+                      quantity, quality and price market.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading14">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(13)}
+                      aria-expanded={this.state.accordion[13]}
+                      aria-controls="collapse14"
+                    >
+                      <h5 className="m-0 p-0">Profit Distribution</h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[13]}
+                    data-parent="#accordion"
+                    id="collapse14"
+                  >
+                    <CardBody>
+                      Profit distribution is on a First Pay, First Out basis.
+                      Those who paid and confirmed first will be the first ones
+                      to receive the profit.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading15">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(14)}
+                      aria-expanded={this.state.accordion[14]}
+                      aria-controls="collapse15"
+                    >
+                      <h5 className="m-0 p-0">How can I withdraw my money?</h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[14]}
+                    data-parent="#accordion"
+                    id="collapse15"
+                  >
+                    <CardBody>
+                      Withdrawal request is only available from the 1st day up
+                      until the 10th day each month. All requests that will be
+                      send after will not be accepted. Submission of withdrawal
+                      request can be send directly using your FarmOn account.
+                      Just log in to your account. Click the withdraw button at
+                      the bottom of the Vault Summary. Enter the amount you want
+                      to withdraw then submit. If you are using someone's bank
+                      account attach a copy of authorization letter from the
+                      account owner then submit your request. Kindly update your
+                      profile to fill up your bank details/money remittance
+                      details. Note: We send withdrawals through BDO, PNB and
+                      Security Bank. Bank or Money Transfers surcharges are
+                      respectfully shouldered by the FarmOners.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading16">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(15)}
+                      aria-expanded={this.state.accordion[15]}
+                      aria-controls="collapse16"
+                    >
+                      <h5 className="m-0 p-0">
+                        When will I received the money I withdraw?
+                      </h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[15]}
+                    data-parent="#accordion"
+                    id="collapse16"
+                  >
+                    <CardBody>
+                      Withdrawal Requests are processed at the end of the month
+                      and will be send to your preferred Bank/Remittance Center.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading17">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(16)}
+                      aria-expanded={this.state.accordion[16]}
+                      aria-controls="collapse17"
+                    >
+                      <h5 className="m-0 p-0">How can I contact you?</h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[16]}
+                    data-parent="#accordion"
+                    id="collapse17"
+                  >
+                    <CardBody>
+                      You can contact us through message center, call us at
+                      (+86)15549443821 or (+86)111 222 3333 or visit us at our
+                      main office located at #555 Shanghai, China. You can also
+                      contact us on our Facebook page but our response may be
+                      delayed.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading18">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(17)}
+                      aria-expanded={this.state.accordion[17]}
+                      aria-controls="collapse18"
+                    >
+                      <h5 className="m-0 p-0">When is the next cycle?</h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[17]}
+                    data-parent="#accordion"
+                    id="collapse18"
+                  >
+                    <CardBody>
+                      The 21st cycle will open on January 14, 2019 until
+                      February 28, 2019.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading19">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(18)}
+                      aria-expanded={this.state.accordion[18]}
+                      aria-controls="collapse19"
+                    >
+                      <h5 className="m-0 p-0">
+                        What is the best product to invest in?
+                      </h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[18]}
+                    data-parent="#accordion"
+                    id="collapse19"
+                  >
+                    <CardBody>
+                      The best product to invest in is the Rice Crop. Why?
+                      Because, FarmOn has complete technology for the production
+                      of good quality rice from planting, harvesting, milling,
+                      up to packaging. FarmOn also has a large number of rice
+                      growers or farmers registered to be funded by FarmOners.
+                      The ROI of rice is estimated from 10% to 20%.
+                    </CardBody>
+                  </Collapse>
+                </Card>
+
+                <Card className="mb-0">
+                  <CardHeader id="heading20">
+                    <Button
+                      block
+                      color="link"
+                      className="text-left m-0 p-0"
+                      onClick={() => this.toggleAccordion(19)}
+                      aria-expanded={this.state.accordion[19]}
+                      aria-controls="collapse20"
+                    >
+                      <h5 className="m-0 p-0">Why rice?</h5>
+                    </Button>
+                  </CardHeader>
+                  <Collapse
+                    isOpen={this.state.accordion[19]}
+                    data-parent="#accordion"
+                    id="collapse20"
+                  >
+                    <CardBody>
+                      Rice has been a staple of Filipino diet since time and
+                      memorial. FarmOn offers Rice product to be funded due to
+                      high and steady demand since Filipinos eat rice every day.
+                      Furthermore, the following reasons should be regarded as
+                      basis for the production of rice:  • FARMON serves over a
+                      thousand farmers that are in need of capital investment on
+                      the production of rice.  • Although rice undergoes a lot
+                      of processes before going to market, it only requires
+                      minimal marketing because consumers are already familiar
+                      with the product.  • Despite the fact that the quality of
+                      rice deteriorates after three months; it does not spoils
+                      easily as those of the vegetable products offered for the
+                      last cycles.  • FarmOn also invests on post-harvest
+                      facilities to ensure the quality of rice. We at FarmOn
+                      Agri-Community Corp., would like to take part on the
+                      contribution for higher rice production and be the leader
+                      of the movement for Philippines to be a rice sufficient
+                      country and we believe that without you, this will not be
+                      possible.
                     </CardBody>
                   </Collapse>
                 </Card>
