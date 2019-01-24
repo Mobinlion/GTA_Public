299490de342f980f20fddaa073ed11004cd26507
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2019-01-24 21:54:31 +0800

    contact page modified

--- pages/contact.js
+++ pages/contact.js
@@ -6,9 +6,19 @@ export default class extends Page {
     return (
       <Layout {...this.props}>
         <h1 className="display-6">Contact Us</h1>
-        <p className="lead">Green Thumb Agriculture</p>
+        <p className="lead">Main Office Address</p>
+        <p>#577 Bund, Shanghai, China</p>
+        <p className="lead">Office Hours</p>
+        <p>
+          Our office is open Monday to Friday from 8:00AM to 5:00PM (GMT +8:00)
+        </p>
 
-        <p>Here goes our Contact Us</p>
+        <p className="lead">Contacts</p>
+        <p>
+          <b>Phone:</b> +86 1111 222 3344
+          <br />
+          <b>Email:</b> info@greenthumb.farm
+        </p>
       </Layout>
     );
   }
