033a75d50c4307082a033c757343a6b587561031
S. Mobin Arablou <mobinlion@yahoo.com>
2019-06-04 15:18:44 +0800

    Revert "ssl, investment page"
    This reverts commit e46c630019f2de708285581dd8180e829ffd6034.

--- pages/farms/farmiloilo.js
+++ pages/farms/farmiloilo.js
@@ -58,7 +58,6 @@ export default class extends Page {
         className="myClassname"
         display="initial"
         position="relative"
-        scrolling="yes"
         allowFullScreen/>
         </div>
       </Layout>
